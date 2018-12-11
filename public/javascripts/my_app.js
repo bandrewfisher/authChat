815d7eb46b2ad96513e8a78214732ca6ff1ad6ea
bandrewfisher <bf6038@gmail.com>
2018-12-11 01:48:19 +0000

    Done

--- public/javascripts/my_app.js
+++ public/javascripts/my_app.js
@@ -10,39 +10,44 @@ controller('myController', ['$scope', '$http',
       $scope.user = {};
       $scope.error = data;
     });
-    
+
     //Display all users in the sidebar
     $scope.users = [];
-    $http.get('/allUsers').success(function(data) {
-      console.log(data);
-      for(var i in data) {
-        var pushUser = {
-          username: data[i].username,
-          profilePic: data[i].profilePic
+    $scope.getUsers = function() {
+      $http.get('/allUsers').success(function(data) {
+        var localUsers = [];
+        console.log(data);
+        for (var i in data) {
+          var pushUser = {
+            username: data[i].username,
+            profilePic: data[i].profilePic
+          }
+         localUsers.push(pushUser);
         }
-        $scope.users.push(pushUser);
-      }
-    })
-    
+        $scope.users = localUsers;
+      })
+    }
+    $scope.getUsers();
+
     $scope.username = "";
     $http.get('/userData').success(function(data) {
       console.log("user data:", data);
       $scope.username = data.username;
     })
-    
-    
+
+
     //For handling new posts
     $scope.postText = "";
     $scope.submitPost = function() {
-      
-      $http.post('/submitPost', {text: $scope.postText}).success(function(data) {
+
+      $http.post('/submitPost', { text: $scope.postText }).success(function(data) {
         console.log("successful post");
         console.log(data);
         $scope.postText = "";
         $scope.getPosts();
       })
     }
-    
+
     //For displaying posts
     $scope.allPosts = [];
     $scope.getPosts = function() {
@@ -51,30 +56,33 @@ controller('myController', ['$scope', '$http',
       })
     }
     $scope.getPosts();
-    
+
     $scope.formatDate = function(timestamp) {
-      var date = new Date(timestamp*1000);
+      var date = new Date(timestamp * 1000);
       var hour = date.getHours();
       var min = date.getMinutes();
-      
+
       var amPm = "am";
-      if(hour > 12) {
+      if (hour > 12) {
         hour -= 12;
         amPm = "pm";
       }
-      if(hour == 0) {
+      if (hour == 0) {
         hour = 12;
       }
-      
+
       return $scope.zeroPad(hour) + ":" + $scope.zeroPad(min) + " " + amPm;
     }
-    
-    $scope.zeroPad= function(number) {
+
+    $scope.zeroPad = function(number) {
       var padding = "";
-      if(number < 10) {
+      if (number < 10) {
         padding = "0";
       }
       return padding + number;
     }
+
+    setInterval($scope.getUsers, 5000);
+    setInterval($scope.getPosts, 5000);
   }
 ]);
